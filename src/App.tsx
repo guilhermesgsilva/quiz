b07e27b1570cf95fef9c1036fefaa82d5017ae6c
Guilherme Silva <silva.gsg@gmail.com>
2025-02-26 13:36:05 +0000

    feat: Create html structure, components state and types

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,56 @@
+import { useState } from "react";
+import { QuestionState, Difficulty, fetchQuizQuestions } from "./API";
 import QuestionCard from "./components/QuestionCard";
+
+type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correctAnswer: string;
+};
+
+const TOTAL_QUESTIONS = 10;
 
 function App() {
   /* https://opentdb.com/api.php?amount=10 */
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState<QuestionState[]>([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+
+  const handleStart = () => {
+    console.log(
+      "🚀 ~ fetchQuizQuestions:",
+      fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY)
+    );
+  };
+
+  const handleAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+
+  const handleNext = () => {};
+
   return (
-    <>
-      Quiz
-      <QuestionCard />
-    </>
+    <div>
+      <h1>Quiz</h1>
+      <button className="start" onClick={handleStart}>
+        Start
+      </button>
+      <p className="score">Score:</p>
+      <p className="score">Loading Questions ...</p>
+      <QuestionCard
+        question={questions[number]?.question}
+        answers={questions[number]?.answers}
+        callback={handleAnswer}
+        userAnswer={userAnswers ? userAnswers[number]?.answer : undefined}
+        questionNumber={number + 1}
+        totalQuestions={TOTAL_QUESTIONS}
+      />
+      <button className="next" onClick={handleNext}>
+        Next Question
+      </button>
+    </div>
   );
 }
 
--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,5 +1,40 @@
-const QuestionCard = () => {
-  return <>Question Card</>;
+type Props = {
+  question: string;
+  answers: string[];
+  callback: any;
+  userAnswer?: string;
+  questionNumber: number;
+  totalQuestions: number;
+};
+
+const QuestionCard: React.FC<Props> = (props) => {
+  let {
+    question,
+    answers,
+    callback,
+    userAnswer,
+    questionNumber,
+    totalQuestions,
+  } = props;
+  return (
+    <div>
+      <p className="number">
+        Question: {questionNumber}/{totalQuestions}
+      </p>
+      <p dangerouslySetInnerHTML={{ __html: question }} />
+      <div>
+        {answers?.map((answer) => {
+          return (
+            <div>
+              <button disabled={!!userAnswer} onClick={callback}>
+                <span dangerouslySetInnerHTML={{ __html: answer }}></span>
+              </button>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default QuestionCard;
